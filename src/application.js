fe985a5cb11965de2ebad2ae94b4785780668c06
Steve McGuire <steveorsomethin@gmail.com>
2014-04-17 21:16:43 -0700

    Initial POC

--- src/application.js
+++ src/application.js
@@ -1,50 +1,35 @@
+// http://jsperf.com/complex-loop-unrolling
 /*
  * Main application entry point
  */
-define(['three', 'threeStats'],
-function(THREE, Stats) {
+define(['three', 'threeStats', './math/matrix'],
+function(THREE, Stats, matrix) {
     var innerWidth = window.innerWidth,
         innerHeight = window.innerHeight,
-        container = document.getElementById('shell'),
+        shell = document.getElementById('shell'),
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
         stats = new Stats(),
-        camera = new THREE.PerspectiveCamera(70, innerWidth / innerHeight, 1, 1000),
-        scene = new THREE.Scene(),
-        renderer = new THREE.WebGLRenderer(),
-        geometry = new THREE.CubeGeometry(200, 200, 200),
-        material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5}),
-        cube = new THREE.Mesh(geometry, material),
-        hex,
-        i;
+        deg = 0,
+        points = [[200, 200, 0], [400, 200, 0], [300, 400, 0]];
 
-    for (i = 0; i < geometry.faces.length; i += 2) {
-        hex = Math.random() * 0xffffff;
-        geometry.faces[i].color.setHex(hex);
-        geometry.faces[i + 1].color.setHex(hex);
-    }
-
-    camera.position.x = -300;
-    camera.position.y = 300;
-    camera.position.z = 1000;
-
-    cube.position.y = 150;
-    scene.add(cube);
-
-    renderer.setSize(innerWidth, innerHeight);
-    container.appendChild(renderer.domElement);
+    onWindowResize();
+    shell.appendChild(canvas);
 
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
-    container.appendChild(stats.domElement);
+    shell.appendChild(stats.domElement);
 
     window.addEventListener('resize', onWindowResize, false);
 
     animate();
 
     function onWindowResize() {
-        camera.aspect = innerWidth / innerHeight;
-        camera.updateProjectionMatrix();
+        innerWidth = window.innerWidth;
+        innerHeight = window.innerHeight;
 
-        renderer.setSize(innerWidth, innerHeight);
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
     }
 
     function animate() {
@@ -54,8 +39,34 @@ function(THREE, Stats) {
     }
 
     function render() {
-        renderer.render(scene, camera);
+        var degrees = deg++,
+            rotX = matrix.makeRotationX(degrees),
+            rotY = matrix.makeRotationY(degrees),
+            rotZ = matrix.makeRotationZ(degrees),
+            rot = matrix.multiply(rotZ, matrix.multiply(rotX, rotY)),
+            trans = matrix.makeTranslation(-300, -300, -50),
+            invTrans = matrix.makeTranslation(300, 300, 50),
+            rotTrans = matrix.multiply(rot, trans),
+            mat = matrix.multiply(invTrans, rotTrans),
+            p1 = matrix.multiplyVector(mat, points[0]),
+            p2 = matrix.multiplyVector(mat, points[1]),
+            p3 = matrix.multiplyVector(mat, points[2]);
+
+        context.fillStyle = 'rgba(0, 0, 0, 1.0)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        context.fillStyle = 'rgba(200, 0, 0, 1.0)';
+        context.strokeStyle = '#FF0000';
+        context.beginPath();
+        context.moveTo(p1[0], p1[1]);
+        context.lineTo(p2[0], p2[1]);
+        context.lineTo(p3[0], p3[1]);
+        context.closePath();
+        context.stroke();
+        context.fill();
     }
+
+    
 
     console.log('Application started');
 });
---
+++ src/math/matrix.js
@@ -0,0 +1,214 @@
+define([], function() {
+
+    function transpose(m, out) {
+        var length = m.length,
+            mo = out || new Array(length),
+            dimension = Math.sqrt(length),
+            row,
+            col;
+
+        for (col = 0; col < dimension; col++) {
+            for (row = 0; row < dimension; row++) {
+                mo[(dimension * row) + col] = m[(dimension * col) + row];
+            }
+        }
+
+        return mo;
+    }
+
+    function multiply(m1, m2, out) {
+        var length = m1.length,
+            dimension = Math.sqrt(length),
+            outIndex = 0,
+            col,
+            m1Index,
+            m2Index,
+            m1Row,
+            m2Row;
+
+        out = out || makeZeroes(dimension);
+
+        for (m1Row = 0; m1Row < dimension; m1Row++) {
+            for (m2Row = 0; m2Row < dimension; m2Row++, outIndex++) {
+                for (col = 0; col < dimension; col++) {
+                    m1Index = (dimension * m1Row) + col;
+                    m2Index = (dimension * col) + m2Row;
+                    out[outIndex] += m1[m1Index] * m2[m2Index];
+                }
+            }
+        }
+
+        return out;
+    }
+
+    function multiplyVector(m, v, out) {
+        var i = 0,
+            mDimension = Math.sqrt(m.length),
+            vDimension = v.length,
+            row,
+            col;
+
+        out = out || [0, 0, 0];
+
+        if (mDimension - vDimension) {
+            multiplyVectorPadded(m, v, out);
+        } else {
+            for (col = 0; col < mDimension; col++) {
+                for (row = 0; row < mDimension; row++) {
+                    out[col] += v[col] * m[(mDimension * row) + col];
+                }
+            }
+        }
+
+        return out;
+    }
+
+    function multiplyVectorPadded(m, v, out) {
+        var mDimension = Math.sqrt(m.length),
+            lastRowOffset = mDimension * (mDimension - 1),
+            d = 0,
+            row,
+            col;
+
+        for (col = 0; col < mDimension; col++) {
+            d += (v[col] || 1) * m[lastRowOffset + col];
+        }
+
+        d = 1 / d;
+
+        for (row = 0; row < v.length; row++) {
+            for (col = 0; col < mDimension; col++) {
+                out[row] += (v[col] || 1) * m[(mDimension * row) + col];
+            }
+            out[row] *= d;
+        }
+
+        return out;
+    }
+
+    function makeRotationX(degrees) {
+        var radMult = 0.0174532,
+            theta = degrees * radMult,
+            s = Math.sin(theta),
+            c = Math.cos(theta);
+
+        return [
+            1, 0, 0, 0,
+            0, c, -s, 0,
+            0, s, c, 0,
+            0, 0, 0, 1
+        ];
+    }
+
+    function makeRotationY(degrees) {
+        var radMult = 0.0174532,
+            theta = degrees * radMult,
+            s = Math.sin(theta),
+            c = Math.cos(theta);
+
+        return [
+            c, 0, s, 0,
+            0, 1, 0, 0,
+            -s, 0, c, 0,
+            0, 0, 0, 1
+        ];
+    }
+
+    function makeRotationZ(degrees) {
+        var radMult = 0.0174532,
+            theta = degrees * radMult,
+            s = Math.sin(theta),
+            c = Math.cos(theta);
+
+        return [
+            c, -s, 0, 0,
+            s, c, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ];
+    }
+
+    function makeTranslation() {
+        var args = Array.prototype.slice.call(arguments, 0),
+            argLen = args.length,
+            dimension = argLen + 1,
+            out = new Array(dimension * dimension),
+            offset,
+            row,
+            col;
+
+        for (row = 0; row < dimension; row++) {
+            offset = (dimension * row);
+            for (col = 0; col < dimension - 1; col++) {
+                out[offset + col] = row === col ? 1 : 0;
+            }
+            out[offset + col] = row >= argLen ? 1 : args[row];
+        }
+
+        return out;
+    }
+
+    function makeScale() {
+        var args = Array.prototype.slice.call(arguments, 0),
+            argLen = args.length,
+            dimension = argLen + 1,
+            out = new Array(dimension * dimension),
+            offset,
+            row,
+            col;
+
+        for (row = 0; row < dimension; row++) {
+            offset = (dimension * row);
+            for (col = 0; col < dimension; col++) {
+                out[offset + col] = row === col ? args[row] : 0;
+            }
+        }
+
+        out[out.length - 1] = 1;
+
+        return out;
+    }
+
+    function makeIdentity() {
+        var args = Array.prototype.slice.call(arguments, 0),
+            argLen = args.length,
+            dimension = argLen + 1,
+            out = new Array(dimension * dimension),
+            offset,
+            row,
+            col;
+
+        for (row = 0; row < dimension; row++) {
+            offset = (dimension * row);
+            for (col = 0; col < dimension; col++) {
+                out[offset + col] = row === col ? 1 : 0;
+            }
+        }
+
+        return out;
+    }
+
+    function makeZeroes(dimension) {
+        var out = new Array(dimension * dimension),
+            i;
+
+        for (i = 0; i < out.length; i++) {
+            out[i] = 0;
+        }
+
+        return out;
+    }
+
+    return {
+        transpose: transpose,
+        multiply: multiply,
+        multiplyVector: multiplyVector,
+        makeZeroes: makeZeroes,
+        makeIdentity: makeIdentity,
+        makeScale: makeScale,
+        makeTranslation: makeTranslation,
+        makeRotationX: makeRotationX,
+        makeRotationY: makeRotationY,
+        makeRotationZ: makeRotationZ
+    };
+});
